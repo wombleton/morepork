84052f56c2c38566d7b6a1daecffc15a264e4c6f
Ian Randall <ianrandall@Ians-MacBook-Pro.local>
2013-04-06 16:42:14 +1300

    Add jade and create folders: routes and views

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-var log = require('debug_log').debug_log;
+
+/* Module dependencies */
+
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    log = require('debug_log').debug_log;
 
 const PORT_NUMBER = 4949;
 
-var express = require("express");
-
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('bingo');
+app.set('port', process.env.PORT || PORT_NUMBER);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-app.listen(PORT_NUMBER);
-
-log('Server started on port ' + PORT_NUMBER);
